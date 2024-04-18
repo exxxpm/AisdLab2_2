f430b36a54805b119cdf1d6a0c9fc538e7c9bfb7
exxxpm <127376127+exxxpm@users.noreply.github.com>
2024-04-19 02:55:38 +0400

    refactored insert, added insert_or_assign

--- include/hash_table.h
+++ include/hash_table.h
@@ -31,11 +31,13 @@ class hash_table {
 
 	void _grow() {//увеличивает размер хеш таблицы, нужен когда коэф загруженности больше 0.6
 		std::vector<pair<Key, Value>> tmp;
-		tmp.resize(_data.size() * 1.7);
+		tmp.resize((_data.size() + 1)* 1.7);
 		std::swap(tmp, _data);
 		_size = 0;
-		for (auto& pair : tmp) {
-			if (pair.is_filled) insert(pair.key, pair.value);
+		for (auto i = 0; i < tmp.size(); ++i) {
+			if (tmp[i].is_filled) {
+				insert(tmp[i].key, tmp[i].value);
+			}
 		}
 	}
 public:
@@ -46,18 +48,57 @@ public:
 		_data.resize(size);
 	}
 
+	hash_table(size_t size, const Value& min, const Value& max): _size(0){
+		if (!size) {
+			throw std::invalid_argument("invalid size");
+		}
+		_data.resize(size);
+		for (int i = 0; i < size; ++i) {
+			Value value = rand() % (max - min + 1) + min;//рандомное значение
+			_data[i] = pair(i+1, value);
+			++_size;
+			_data[i].is_filled = true;
+		}
+	}
+
 	size_t get_size() { return _size; }
 
-	void insert(const Key& key, const Value& value) {
-		size_t ind = hash(key) % _data.size();
-		auto load_factor = _size / (_data.size() + 0.0);//+0.0 это каст к даблу (считаем коэф загруженности хеш таблицы
-		if (load_factor > 0.6) _grow();
-		for (size_t i = ind; i < _data.size(); ++i)//линейное пробирование с шагом 1
+	void insert(const Key& key, const Value& value)
+	{
+		if (_size) {
+			double load_factor = _size / (_data.size() + 0.0);//считаем коэф загруженности и увеличиваем размер таблицыБ если он больше 0.6
+			if (load_factor > 0.6) _grow();
+		}
+		for (size_t i = 0; i < _data.size(); ++i)
 		{
-			if (_data[ind].key == key) throw std::logic_error("");
-			if (!_data[i].is_filled) {
-				_data[i] = pair(key, value);
+			size_t index = (hash(key) + i * hash(key)) % _data.size();//пробирование с помощью двойного хеширования
+			if (!_data[index].is_filled)
+			{
+				_data[index] = pair(key, value);
 				++_size;
+				return;
+			}
+			if (_data[index].key == key) throw std::logic_error("key is exist");
+		}
+	}
+
+	void insert_or_assign(const Key& key, const Value& value)
+	{
+		if (_size) {
+			double load_factor = _size / (_data.size() + 0.0);
+			if (load_factor > 0.6) _grow();
+		}
+		for (size_t i = 0; i < _data.size(); ++i)
+		{
+			size_t index = (hash(key) + i * hash(key)) % _data.size();//опять считаем индекс методом двойного хеширования
+			if (!_data[index].is_filled)//если нет элемента вставляем, если есть обновляем
+			{
+				_data[index] = pair(key, value);
+				++_size;
+				return;
+			}
+			if (_data[index].key == key) {
+				_data[index].value = value;
 				return;
 			}
 		}
@@ -69,4 +110,11 @@ public:
 		return (first_part >> static_cast<int>(MACHINE_WORD - l));
 	}
 	size_t size() const noexcept { return _size; }
+
+	void print(){
+		for (auto& pair : _data)
+		{
+			if (pair.is_filled) std::cout << pair<< std::endl;
+		}
+	}
 };
--- tests/hash_tests.cc
+++ tests/hash_tests.cc
@@ -13,3 +13,26 @@ TEST(hash_table_test, default_constr) {
 	ht.insert(2, 5);
 	ASSERT_EQ(ht.get_size(),1);
 }
+
+TEST(hash_table_test, rand_constr) {
+	hash_table<int, int> ht(11,2,9);
+	ASSERT_EQ(ht.get_size(), 11);
+}
+
+TEST(hash_table_test, insert) {
+	hash_table<int, int> ht(11, 2, 9);
+	ht.insert(66, 7);
+	ASSERT_EQ(ht.get_size(), 12);
+}
+
+TEST(hash_table_test, assign_with_insert_or_assign) {
+	hash_table<int, int> ht(11, 2, 9);
+	ht.insert_or_assign(7, 505);
+	ASSERT_EQ(ht.get_size(), 11);
+}
+
+TEST(hash_table_test, insert_with_insert_or_assign) {
+	hash_table<int, int> ht(11, 2, 9);
+	ht.insert_or_assign(505, 505);
+	ASSERT_EQ(ht.get_size(), 12);
+}
